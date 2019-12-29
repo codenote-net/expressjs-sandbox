aca29bb419075f7fc7f48c091dadbeaeeef24e62
Tadashi Shigeoka <shigeoka@codenote.net>
2019-12-29 22:24:03 +0900

    eslint . --fix

--- routes/csv.js
+++ routes/csv.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 
 router.post('/upload', upload.single('csv'), (req, res, next) => {
-  res.send('Uploaded a CSV file');
-});
+  res.send('Uploaded a CSV file')
+})
 
-module.exports = router;
+module.exports = router
